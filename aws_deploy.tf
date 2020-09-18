3fc3024d825f2c177895a4cc1a639d15cde465f8
hemant kumar <techhb@gmail.com>
2020-09-18 15:48:16 -0400

    Update aws_deploy.tf

--- aws_deploy.tf
+++ aws_deploy.tf
@@ -4,7 +4,11 @@ provider "aws" {
 
 
 }
+terraform {
+  required_version = "~> 0.12.0"
 
+  backend "remote" {}
+}
 resource "aws_instance" "myec2" {
   ami = "ami-0603cbe34fd08cb81"
   instance_type = "t2.micro"
