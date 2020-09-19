ca8f9bd96e5a522fd54d213066cafcc0b863653b
MacBook Pro <macbookpro@MacBooksMacBook.attlocal.net>
2020-09-18 23:15:02 -0400

    New Confs

--- aws_deploy.tf
+++ aws_deploy.tf
@@ -14,10 +14,17 @@ terraform {
   }
 }
 
-resource "aws_instance" "myec2" {
-  ami = "ami-0603cbe34fd08cb81"
+
+resource "aws_instance" "test-ec2-instance" {
+  ami = "${var.ami_id}"
   instance_type = "t2.micro"
+  key_name = "${var.ami_key_pair_name}"
+  security_groups = ["${aws_security_group.ingress-all-test.id}"]
+tags {
+    Name = "${var.ami_name}"
+  }
+subnet_id = "${aws_subnet.subnet-uno.id}"
 }
 output "public_ip"{
-  value =aws_instance.myec2.public_ip
+  value =aws_instance.test-ec2-instance.public_ip
 }
---
+++ terraform.tfvars
@@ -0,0 +1,13 @@
+variables.tf
+variable "ami_name" {
+  type: string
+  default: "Jenkins Build Server"
+}
+variable "ami_id" {
+  type = string
+  default: "ami-0603cbe34fd08cb81"
+}
+variable "ami_key_pair_name" {
+  type = string
+  default: "ec2_docker_install.pem"
+}
