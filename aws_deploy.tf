73c3d5aa449a8ccbdd016cbbe74dc17875c5a72f
MacBook Pro <macbookpro@MacBooksMacBook.attlocal.net>
2020-09-18 22:22:23 -0400

    Adding EC Instance

--- aws_deploy.tf
+++ aws_deploy.tf
@@ -14,11 +14,10 @@ terraform {
   }
 }
 
-#resource "aws_instance" "myec2" {
-#  ami = "ami-0603cbe34fd08cb81"
-#  instance_type = "t2.micro"
-
-#}
+resource "aws_instance" "myec2" {
+  ami = "ami-0603cbe34fd08cb81"
+  instance_type = "t2.micro"
+}
 output "public_ip"{
   value =aws_instance.myec2.public_ip
 }
