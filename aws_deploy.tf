f3d5acc0743e4889b5abe2064a1ed5e10e378610
MacBook Pro <macbookpro@MacBooksMacBook.attlocal.net>
2020-09-18 23:24:55 -0400

    Add Data

--- aws_deploy.tf
+++ aws_deploy.tf
@@ -16,9 +16,9 @@ terraform {
 
 
 resource "aws_instance" "test-ec2-instance" {
-  ami = "${var.ami_id}"
+  ami = var.ami_id
   instance_type = "t2.micro"
-  key_name = "${var.ami_key_pair_name}"
+  key_name = var.ami_key_pair_name
   security_groups = ["${aws_security_group.ingress-all-test.id}"]
 subnet_id = "${aws_subnet.subnet-uno.id}"
 }
