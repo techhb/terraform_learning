08ae02d16ac0961bdff85c0fa58cf89432ae6180
MacBook Pro <macbookpro@MacBooksMacBook.attlocal.net>
2020-09-18 23:44:26 -0400

    Adding subnet vpn

--- aws_deploy.tf
+++ aws_deploy.tf
@@ -20,7 +20,7 @@ resource "aws_instance" "test-ec2-instance" {
   instance_type = "t2.micro"
   key_name = var.ami_key_pair_name
   security_groups = [aws_security_group.ingress-all-test.id]
-subnet_id = "aws_subnet.subnet-uno.id"
+subnet_id = aws_subnet.subnet-uno.id
 }
 output "public_ip"{
   value = aws_instance.test-ec2-instance.public_ip
