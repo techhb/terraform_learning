fc573f0b0c7ccb53ca7597e98afbdfea36e85c28
MacBook Pro <macbookpro@MacBooksMacBook.attlocal.net>
2020-09-19 10:37:34 -0400

    Adding Internet Gateway and Resources

--- aws_deploy.tf
+++ aws_deploy.tf
@@ -14,7 +14,7 @@ terraform {
   }
 }
 
-/*
+
 resource "aws_instance" "test-ec2-instance" {
   ami = var.ami_id
   instance_type = "t2.micro"
@@ -25,7 +25,7 @@ subnet_id = aws_subnet.subnet-uno.id
 output "public_ip"{
   value = aws_instance.test-ec2-instance.public_ip
 }
-
+/*
 resource "aws_key_pair" "ec2_conn" {
   key_name   = "ec2_conn"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCYfCAXkNBPYyq5HqZjeNJUFDB6TZcGm6KKJXKpgL5zqFbg/u5ao0z0sWt8Cz6z5lKGs/kjdbd4N8+H0Z/42CuYyB+JT0jMo3p/tiCpDuiLPJECpjyMm7wjYpIBVd6PPORo+WU5h/iG3FeoHmSFjJJyYHaBT0kJ9ElSemsL+R1POg+bpO8OIGa2EGHgWFqx2wiFbQE5Jwr6jzetJRnR1elo1ppr7z78z1cqu8mLenoYzBQzJ3yn9CRL79P0aaAEgQonfn+iNdyRN9XY8JZix0eEtG/mB1RQIUD4VZ5Vk/SESUZ07JrUzHmp7K+PHCw9+2ITGUxOOxMVlN2z53tcJQYH"
