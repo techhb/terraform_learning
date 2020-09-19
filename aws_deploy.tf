34942de047c001aade37781c9ea1872d430a33ee
MacBook Pro <macbookpro@MacBooksMacBook.attlocal.net>
2020-09-18 23:32:41 -0400

    Messages

--- aws_deploy.tf
+++ aws_deploy.tf
@@ -19,7 +19,7 @@ resource "aws_instance" "test-ec2-instance" {
   ami = var.ami_id
   instance_type = "t2.micro"
   key_name = var.ami_key_pair_name
-  security_groups = ["$aws_security_group.ingress-all-test.id}"]
+  security_groups = ["${aws_security_group.ingress-all-test.id}"]
 subnet_id = "${aws_subnet.subnet-uno.id}"
 }
 output "public_ip"{
