a7ba36268bd0fe2e1b0e8bba67e2782b331b4678
MacBook Pro <macbookpro@MacBooksMacBook.attlocal.net>
2020-09-19 00:33:41 -0400

    Adding Internet Gateway and Resources

--- subnets.tf
+++ subnets.tf
@@ -9,9 +9,7 @@ route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.test-env-gw.id
   }
-tags {
-    Name = "test-env-route-table"
-  }
+
 }
 resource "aws_route_table_association" "subnet-association" {
   subnet_id      = aws_subnet.subnet-uno.id
